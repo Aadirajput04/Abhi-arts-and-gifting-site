b6377fa6916d30e9625b7b1b52da07491106a8c3
aaditya singh <aadityasingh396@gmail.com>
2023-10-04 12:14:16 +0530

    half done

---
+++ about.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+
+    <link rel="stylesheet" href="./style.css">
+    <title>About | abhi arts and gifts</title>
+</head>
+
+<body>
+
+
+
+    <section class="head-lable">
+
+
+
+        <?php
+    
+    include "header.php";
+
+    ?>
+
+
+
+
+        <div class="heading-text side-padding">
+            <h1><span> About us</span></h1>
+            <hr>
+        </div>
+
+
+
+
+    </section>
+
+
+
+
+
+sec
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php
+    
+    include "testimonial-section.php";
+
+    ?>
+
+
+
+
+
+    <?php
+    
+    include "contact-section.php";
+
+    ?>
+
+
+    <?php
+    
+    include "footer.php";
+
+    ?>
+</body>
+
+</html>
---
+++ testimonial-section.php
@@ -0,0 +1,275 @@
+<div class="swiper mySwiper ">
+    <img class="comaaaaa" src="./img/comaaaaaa.png" alt="">
+    <!-- <img class="round-eclips" src="./img/Ellipse 6.png" alt=""> -->
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <div class="testimonial-box">
+               
+
+                <div class="left-testi-content">
+                
+
+
+
+                    <h3>Testimonial</h3>
+                    <hr>
+                    <h1>Heartfelt <span> Testimonials from </span>Our Happy Gift Recipients !</h1>
+                    <p>We pLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sapien arcu, molestie ut
+                        eros
+                        pharetra, elementum semper ipsum. Duis in nulla gravida, tincidunt lorem nec, interdum arcu.
+                        Morbi
+                        aliquet vitae libero at rhoncus. Sed nunc tortor, mollis at tristique ut, aliquam in nisl.
+                    </p>
+                    <h6>-James Carder</h6>
+                </div>
+                <div class="right-img">
+                    <img src="./img/testi-pic.png" alt="">
+                </div>
+
+
+            </div>
+
+
+        </div>
+        <div class="swiper-slide">
+            <div class="testimonial-box">
+                <div class="left-testi-content">
+
+
+
+
+                    <h3>Testimonial</h3>
+                    <hr>
+                    <h1>Heartfelt <span> Testimonials from </span>Our Happy Gift Recipients !</h1>
+                    <p>We pLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sapien arcu, molestie ut eros
+                        pharetra, elementum semper ipsum. Duis in nulla gravida, tincidunt lorem nec, interdum arcu.
+                        Morbi
+                        aliquet vitae libero at rhoncus. Sed nunc tortor, mollis at tristique ut, aliquam in nisl.</p>
+                    <h6>-James Carder</h6>
+                </div>
+                <div class="right-img">
+                    <img src="./img/testi-pic.png" alt="">
+                </div>
+            </div>
+
+
+
+
+        </div>
+        <div class="swiper-slide">
+            <div class="testimonial-box">
+                <div class="left-testi-content">
+
+
+
+
+                    <h3>Testimonial</h3>
+                    <hr>
+                    <h1>Heartfelt <span> Testimonials from </span>Our Happy Gift Recipients !</h1>
+                    <p>We pLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sapien arcu, molestie ut eros
+                        pharetra, elementum semper ipsum. Duis in nulla gravida, tincidunt lorem nec, interdum arcu.
+                        Morbi
+                        aliquet vitae libero at rhoncus. Sed nunc tortor, mollis at tristique ut, aliquam in nisl.</p>
+                    <h6>-James Carder</h6>
+                </div>
+                <div class="right-img">
+                    <img src="./img/testi-pic.png" alt="">
+                </div>
+            </div>
+
+
+
+
+        </div>
+        <div class="swiper-slide">
+            <div class="testimonial-box">
+                <div class="left-testi-content">
+
+
+
+
+                    <h3>Testimonial</h3>
+                    <hr>
+                    <h1>Heartfelt <span> Testimonials from </span>Our Happy Gift Recipients !</h1>
+                    <p>We pLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sapien arcu, molestie ut eros
+                        pharetra, elementum semper ipsum. Duis in nulla gravida, tincidunt lorem nec, interdum arcu.
+                        Morbi
+                        aliquet vitae libero at rhoncus. Sed nunc tortor, mollis at tristique ut, aliquam in nisl.</p>
+                    <h6>-James Carder</h6>
+                </div>
+                <div class="right-img">
+                    <img src="./img/testi-pic.png" alt="">
+                </div>
+            </div>
+
+
+
+
+        </div>
+        <div class="swiper-slide">
+            <div class="testimonial-box">
+                <div class="left-testi-content">
+
+
+
+
+                    <h3>Testimonial</h3>
+                    <hr>
+                    <h1>Heartfelt <span> Testimonials from </span>Our Happy Gift Recipients !</h1>
+                    <p>We pLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sapien arcu, molestie ut eros
+                        pharetra, elementum semper ipsum. Duis in nulla gravida, tincidunt lorem nec, interdum arcu.
+                        Morbi
+                        aliquet vitae libero at rhoncus. Sed nunc tortor, mollis at tristique ut, aliquam in nisl.</p>
+                    <h6>-James Carder</h6>
+                </div>
+                <div class="right-img">
+                    <img src="./img/testi-pic.png" alt="">
+                </div>
+            </div>
+
+
+
+
+        </div>
+        <div class="swiper-slide">
+            <div class="testimonial-box">
+                <div class="left-testi-content">
+
+
+
+
+                    <h3>Testimonial</h3>
+                    <hr>
+                    <h1>Heartfelt <span> Testimonials from </span>Our Happy Gift Recipients !</h1>
+                    <p>We pLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sapien arcu, molestie ut eros
+                        pharetra, elementum semper ipsum. Duis in nulla gravida, tincidunt lorem nec, interdum arcu.
+                        Morbi
+                        aliquet vitae libero at rhoncus. Sed nunc tortor, mollis at tristique ut, aliquam in nisl.</p>
+                    <h6>-James Carder</h6>
+                </div>
+                <div class="right-img">
+                    <img src="./img/testi-pic.png" alt="">
+                </div>
+            </div>
+
+
+
+
+        </div>
+        <div class="swiper-slide">
+            <div class="testimonial-box">
+                <div class="left-testi-content">
+
+
+
+
+                    <h3>Testimonial</h3>
+                    <hr>
+                    <h1>Heartfelt <span> Testimonials from </span>Our Happy Gift Recipients !</h1>
+                    <p>We pLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sapien arcu, molestie ut eros
+                        pharetra, elementum semper ipsum. Duis in nulla gravida, tincidunt lorem nec, interdum arcu.
+                        Morbi
+                        aliquet vitae libero at rhoncus. Sed nunc tortor, mollis at tristique ut, aliquam in nisl.</p>
+                    <h6>-James Carder</h6>
+                </div>
+                <div class="right-img">
+                    <img src="./img/testi-pic.png" alt="">
+                </div>
+            </div>
+
+
+
+
+        </div>
+        <div class="swiper-slide">
+            <div class="testimonial-box">
+                <div class="left-testi-content">
+
+
+
+
+                    <h3>Testimonial</h3>
+                    <hr>
+                    <h1>Heartfelt <span> Testimonials from </span>Our Happy Gift Recipients !</h1>
+                    <p>We pLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sapien arcu, molestie ut eros
+                        pharetra, elementum semper ipsum. Duis in nulla gravida, tincidunt lorem nec, interdum arcu.
+                        Morbi
+                        aliquet vitae libero at rhoncus. Sed nunc tortor, mollis at tristique ut, aliquam in nisl.</p>
+                    <h6>-James Carder</h6>
+                </div>
+                <div class="right-img">
+                    <img src="./img/testi-pic.png" alt="">
+                </div>
+            </div>
+
+
+
+
+        </div>
+        <div class="swiper-slide">
+            <div class="testimonial-box">
+                <div class="left-testi-content">
+
+
+
+
+                    <h3>Testimonial</h3>
+                    <hr>
+                    <h1>Heartfelt <span> Testimonials from </span>Our Happy Gift Recipients !</h1>
+                    <p>We pLorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sapien arcu, molestie ut eros
+                        pharetra, elementum semper ipsum. Duis in nulla gravida, tincidunt lorem nec, interdum arcu.
+                        Morbi
+                        aliquet vitae libero at rhoncus. Sed nunc tortor, mollis at tristique ut, aliquam in nisl.</p>
+                    <h6>-James Carder</h6>
+                </div>
+                <div class="right-img">
+                    <img src="./img/testi-pic.png" alt="">
+                </div>
+            </div>
+
+
+
+
+        </div>
+    </div>
+    <div class="swiper-pagination"></div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+<!-- Initialize Swiper -->
+<script>
+    var swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+        },
+    });
+</script>
